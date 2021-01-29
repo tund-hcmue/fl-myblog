884ef5507ccc35a243ea14e6c6325693f40b68e4
tund <tund@pop-os.localdomain>
2021-01-29 20:22:11 +0700

    template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user ={'username': 'Tund'}
+    posts = [
+        {
+            'author': {'username': 'Nguyen'},
+            'body': 'Flask that la de hoc...'
+        },
+        {
+            'suthor': {'username': Long},
+            'body': 'Lap tring web voi Flask...'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
