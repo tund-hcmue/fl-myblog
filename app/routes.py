dcbcff3edd99c167ec159eaf8ddd937c91e20ba2
tund <tund@pop-os.localdomain>
2021-01-30 09:48:17 +0700

    web form - login form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
+
 def index():
     user ={'username': 'Tund'}
     posts = [
@@ -10,8 +13,17 @@ def index():
             'body': 'Flask that la de hoc...'
         },
         {
-            'suthor': {'username': Long},
+            'author': {'username': 'Long'},
             'body': 'Lap tring web voi Flask...'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Yeu cau dang nhap tu user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
