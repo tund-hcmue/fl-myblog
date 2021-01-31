ff94f83b7ce310e29df72d41bb60fd6c58581b0c
tund <tund@pop-os.localdomain>
2021-01-31 18:21:59 +0700

    profile - avatar

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,13 @@ from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import logout_user, login_required, current_user, login_user
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request #goi ham nay truoc khi goi ham hien thi
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
